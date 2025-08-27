efc468c91ee0bf48c36ef46dd0273e484020dd87
Lloffert <sailorlloffert@gmail.com>
2025-08-27 15:20:58 +0000

    Added closing shopping message

--- m1lab.cpp
+++ m1lab.cpp
@@ -21,10 +21,13 @@ int main() {
     cout << "Welcome to the " << item_name << " store!" << endl;
     cout << "Each  " << item_name << " is $" << cost_per << endl;
     cout << "We have " << num_items << " total." << endl;
-    
+
     // do the processing
     double total_cost = num_items * cost_per;
-
+    
+    // Print the output
+    cout << "Buying them all will cost: $" << total_cost << endl;
+    cout << "Thanks for shopping with us!" << endl;
 
     // end program
     return 0;
