519988b36dc22cd4fb70305c2c408210cdc291bc
Lloffert <sailorlloffert@gmail.com>
2025-08-27 14:53:06 +0000

    Adding price and item variables

--- m1lab.cpp
+++ m1lab.cpp
@@ -17,10 +17,14 @@ int main() {
     int    num_items = 10;
     double cost_per = 0.25;
 
+    // Give our sales pitch
     cout << "Welcome to the " << item_name << " store!" << endl;
+    cout << "Each  " << item_name << " is $" << cost_per << endl;
+    cout << "We have " << num_items << " total." << endl;
+    
     // do the processing
     double total_cost = num_items * cost_per;
-    
+
 
     // end program
     return 0;
