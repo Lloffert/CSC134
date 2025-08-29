85fa9e4db56c0a5c089fc11dc7d0e5f31ca6c5f8
Lloffert <sailorlloffert@gmail.com>
2025-08-29 16:38:36 +0000

    added first 3 sentences

--- M1HW1_Loffert.cpp
+++ M1HW1_Loffert.cpp
@@ -17,6 +17,16 @@ int main() {
     int rls_date = 1989;
     double budget = 6.5;
 
+    // Write about movie
+    cout << "\"" << title << "\"" ", also known as \"UFO Abduction\", is a " << rls_date << " sci-fi horror film about the" << endl;
+    cout << "fictional Van Heese family's nightmarish encounter with extraterrestrials, all seen from the" << endl; 
+    cout << "recovered VHS footage of the youngest son's camcorder." << endl;
+    cout << endl;
+
+    cout << "The movie was made on a microbudget of $" << budget << "K dollars and was only ever released on video." << endl;
+    cout << "It's considered to be one of the few early films utilizing the \"found footage\" genre before" << endl;
+    cout << "it was popularised by The Blair Witch Project a decade later." << endl;
+
     // end program
     return 0;
 
