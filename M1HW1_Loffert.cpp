8a3227b514c27220389309c7cef8e05eef751236
Lloffert <sailorlloffert@gmail.com>
2025-08-29 16:28:47 +0000

    Start M1HW1

---
+++ M1HW1_Loffert.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134
+M1HW1
+Lydia Loffert
+8/29/2025
+purpose: Talk about a favorite movie
+*/
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // end program
+    return 0;
+
+}
