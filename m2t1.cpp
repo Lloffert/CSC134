cf581ec2c1de0029660311db87019e8852499289
Lloffert <sailorlloffert@gmail.com>
2025-09-03 14:32:37 +0000

    Started M2T1

---
+++ m2t1.cpp
@@ -0,0 +1,14 @@
+/*
+CSC 134 M2T1
+Lydia Loffert
+9/3/2025
+Revision of "sales" program from M1, now with user input
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
