7762a8a8bd3a4a5e738a2bdd1bf1ba96d37e57df
Lloffert <sailorlloffert@gmail.com>
2025-09-15 15:18:52 +0000

    Added tax info and proper printout

--- M2T2.cpp
+++ M2T2.cpp
@@ -7,9 +7,8 @@ Purpose: Generate a "good receipt"
 */
 
 #include <iostream>
-using namespace std;
 #include <iomanip>
-
+using namespace std;
 
 int main() {
     // Declare variables
@@ -21,14 +20,15 @@ int main() {
 
     // Calculate the values
     cout << "Order up" << endl;
-    cout << item << " (x1)" << endl;
+    cout << item << " (x1)";
     cout << "\t$" << menu_price << endl;
 
-    tax_amount + menu_price * tax_percent; // add sales tax
-    total_price + menu_price + tax_amount;
+    tax_amount = menu_price * tax_percent; // add sales tax
+    total_price = menu_price + tax_amount;
 
     // Print the results
     cout << setprecision(2) << fixed; // Set 2 decimal places
     cout << "Tax: \t\t$" << tax_amount << endl;
+    cout << "_____________________" << endl;
     cout << "Total:\t\t$" << total_price << endl;
 }
