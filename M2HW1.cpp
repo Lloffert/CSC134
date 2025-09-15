35ef46ff8764f162a9363e40271b454107fad7d8
Lloffert <sailorlloffert@gmail.com>
2025-09-15 14:47:46 +0000

    Started M2HW1

---
+++ M2HW1.cpp
@@ -0,0 +1,32 @@
+/*
+CSC 134
+M2HW1 - Silver
+Lydia Loffert
+9/15/2025
+*/
+
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+void question1() {
+    cout << "Question 1" << endl;
+}
+void question2() {
+    cout << "Question 2" << endl;
+}
+
+int main() {
+    // Call each question as its own function
+
+    question1();
+    question2();
+
+    cout << "Example of printing out correct money values" << endl;
+    double cost = 8.5; // $8.50
+    // magic words for 2 decimal places
+    
+    cout << setprecision(2) << fixed; // requires "#include <iomanip>"
+    cout << "The cost is: $" << cost << endl;
+    return 0; // no errors
+}
