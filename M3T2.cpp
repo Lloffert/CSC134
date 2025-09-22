be79e619074254a7b7c9c89aaebcdb10273e0448
Lloffert <sailorlloffert@gmail.com>
2025-09-22 14:39:57 +0000

    m3t2 base skeleton

---
+++ M3T2.cpp
@@ -0,0 +1,29 @@
+/*
+M3T2 - Craps Part 1
+CSC 134
+Lydia Loffert
+9/22/2025
+Beginning of craps game
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// DECLARE Helper functions (todo)
+int roll();
+
+// main
+int main() {
+    int num = roll();
+    cout << num << endl;
+    return 0;
+}
+// DEFINE Helper functions
+int roll() {
+    // rolls a six sided die
+    // TODO
+    return 6;
+}
