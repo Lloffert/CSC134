91498698c30fbc13cda33a0e8de150290b1f6693
Lloffert <sailorlloffert@gmail.com>
2025-09-29 15:26:35 +0000

    Finished M3LAB2 with working dice roll

--- M3LAB2.cpp
+++ M3LAB2.cpp
@@ -14,12 +14,13 @@ using namespace std;
 // DECLARE the functions here
 void letter_grader();
 void combat();
+int roll();
 
 int main()
 {
-
-    letter_grader();
-    // combat;
+    // letter_grader();
+    combat();
+    roll();
     return 9;
 }
 
@@ -61,4 +62,46 @@ void letter_grader() {
     cout << "A number grade of " << num_grade << " is: " << letter_grade;
     cout << endl << endl;
     
+}
+
+void combat() {
+    /*
+    A simple D&D style combat demo.
+    Attack roll + bonus >= armor class? Then hit, else miss
+    */
+
+   //variables
+   int attack_roll, attack_bonus, enemy_armor;
+
+   cout << "You are fighting a goblin." << endl;
+   cout << "Enter attack bonus: ";
+   cin >> attack_bonus;
+   cout << "Enemy armor class: ";
+   cin >> enemy_armor;
+
+   // Roll to hit
+   attack_roll = roll();
+   cout << "Roll: " << attack_roll << " + " << attack_bonus << " = " << attack_roll + attack_bonus << endl;
+   if (attack_roll + attack_bonus >= enemy_armor) {
+    cout << "hit!" << endl;
+   }
+   else {
+    cout << "Miss!" << endl;
+   }
+
+   // try again?
+   cout << "\nAgain? (y/n): ";
+   string again;
+   cin >> again;
+   if (again == "y" ) {
+    // just call the function again
+    combat();
+   }
+}
+
+int roll() {
+    const int SIDES = 20;
+    int my_roll;
+    my_roll = (rand() % SIDES) + 1;
+    return my_roll;
 }
