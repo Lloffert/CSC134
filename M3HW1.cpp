da6b18cdf97ee841dd0f3e88af776c3b5592e5a7
Lloffert <sailorlloffert@gmail.com>
2025-10-10 16:57:30 +0000

    CYOA question fully working

--- M3HW1.cpp
+++ M3HW1.cpp
@@ -85,16 +85,28 @@ void ChatBot() { // chatbot start
     void Branch_1() 
     { // branch1 start
 
-        cout << "\nPLACEHOLDER START\n\n";
-        cout << "Type 1 or 2:\n";
+        cout << "Stuck in the Bathroom Simulator: \n\n";
+        cout << "You're in a bathroom stall at the airport, and you're completely out of toilet paper.\n";
+        cout << "Looking at the gaps between the stalls, it doesn't look like there's anyone to your \n";
+        cout << "left, and on your right there's somebody in there but he's got no shoes on. What now?\n\n";
+
+
+        cout << "(Type 1 or 2:)\n";
+        cout << "(1) Peek over empty stall to see if there's paper (2) Ask the barefoot guy ";
         int Choice1;
         cin >> Choice1;
 
         if (Choice1 == 1) {
-            cout << "\nPLACEHOLDER LOSE\n"; // GAME OVER              
+            cout << "\nYou stand up on the toilet to peek over the stall and Agh! There was somebody in\n"; // GAME OVER  
+            cout << "there! He yelps and clobbers you over the head faster than you can apologize, and \n";
+            cout << "I guess you die in there, from humiliation or head injury.\n\n";  
+            cout << "** GAME OVER **\n\n";          
         }
         else if (Choice1 == 2) {
-            cout << "\nPLACEHOLDER NEXT\n\n";
+            cout << "\nYou hesitantly knock on the right-hand stall and ask for some toilet paper.\n\n";
+            cout << "\"Yeah sure I got some\" he says, and from the left stall that you thought was empty\n";
+            cout << "someone else says \"I got toilet paper\". Holy smokes there was somebody in there,\n";
+            cout << "you're glad you didn't peek over!";
             Branch_2(); // GOTO BRANCH 2
         }
         else {
@@ -105,17 +117,26 @@ void ChatBot() { // chatbot start
     void Branch_2()
     { // branch2 start
 
-        pause();
-        cout << "PLACEHOLDER START\n\n";
-        cout << "Type 1 or 2:\n";
+
+        cout << " Who's toilet paper do you choose?\n\n";
+        cout << "(Type 1 or 2:)\n";
+        cout << "(1) Normal guy (2) Barefoot guy ";
         int Choice2;
         cin >> Choice2;
 
-        if (Choice2 == 1) {
-            cout << "PLACEHOLDER WIN"; // BEAT THE GAME
+        if (Choice2 == 1) { // BEAT THE GAME
+            cout << "\nYou ask the man on the left for his paper and he passes it over. All three of you\n"; 
+            cout << "flush and exit at the same time and make no eye contact as you wash your hands and\n";
+            cout << "leave for your flight.\n\n";
+            cout << "Good job getting out of there, you won!\n\n";
         }
-        else if (Choice2 == 2) {
-            cout << "PLACEHOLDER LOSE"; // GAME OVER
+        else if (Choice2 == 2) { // GAME OVER
+            cout << "\nThe barefoot man passes something over and you hear both of the men flush and exit\n"; 
+            cout << "the bathroom. Wait a minute he just gave you a crumpled up receipt! What are you\n";
+            cout << "supposed to do with this?!\n\n";
+            cout << "Sorry, looks like you're stuck in here until the next person comes in, hopefully you\n";
+            cout << "don't miss your flight...\n\n";
+            cout << "** GAME OVER **\n\n";
         }
         else {
             cout << "PLEASE CHOOSE 1 OR 2";
