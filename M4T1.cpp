8376c43a839a97d21afed1257332594a6e4edd6e
Lloffert <sailorlloffert@gmail.com>
2025-10-18 22:12:54 +0000

    Completed M4T1

---
+++ M4T1.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// M4T1 - While
+// Lydia Loffert
+// 10/18/2025
+
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+
+    // Part 1, just say "hi" five times
+    int count = 1;
+    while (count < 6) 
+    {
+        cout << "Hello number " << count << endl;
+        ++count;
+    }
+
+    return 0; // done
+}
