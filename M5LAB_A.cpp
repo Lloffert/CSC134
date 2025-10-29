112b6b24b2d88003c0b32e9d8860e369a6a65e5c
Lloffert <sailorlloffert@gmail.com>
2025-10-29 15:36:41 +0000

    Added emoji visual to health regen

--- M5LAB_A.cpp
+++ M5LAB_A.cpp
@@ -15,14 +15,27 @@ using namespace std;
 int main() {
     // TODO: Put healing code into its own function
     int maxHP = 100;
-    int hp    = 30;
+    int hp    = 10;
     const int healing = 10;
+
+    // characters to show the health bar
+    string full = "❤️ "; // Full block
+
+    // Empty portion (remaining):
+    string empty = "🖤";
 
     cout << "Resting until healed." << endl;
     while (hp < maxHP) {
         hp += healing;
         cout << "HP: " << hp << "/" << maxHP << endl;
         
+        for (int i=0 ; i < hp/10; i++) {
+            cout << full;
+        }
+        for (int i=0; i < (100-hp)/10; i++) {
+            cout << empty;
+        }
+        cout << " HP: " << hp << "/" << maxHP << endl;
     }
     cout << "Fully rested." << endl;
 
