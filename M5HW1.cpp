9d0e6ef7eb313556609bc7c1cad77792777ddbd2
Lloffert <sailorlloffert@gmail.com>
2025-11-03 16:34:55 +0000

    Started M5HW1

---
+++ M5HW1.cpp
@@ -0,0 +1,52 @@
+/*
+M5HW1 - Silver
+CSC 134
+Lydia Loffert
+11/3/2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Question 1: 
+    // *Calculate average rainfall for three months, 
+    // *asks user for the name of each month
+    // *ask amount of rain (in inches) that fell each month
+    // *display average (rounded to 2 decimal points)
+
+    cout << "QUESTION 1\n";
+    cout << "----------\n\n";
+
+    // rain variables
+    string month1;
+    string month2;
+    string month3;
+    double rainfall1;
+    double rainfall2;
+    double rainfall3;
+
+    // Get rainfall info from user
+    cout << "Enter month: ";
+    cin >> month1;
+    cout << "Enter rainfall for " << month1 << ": ";
+    cin >> rainfall1;
+
+    cout << "\nEnter month: ";
+    cin >> month2;
+    cout << "Enter rainfall for " << month2 << ": ";
+    cin >> rainfall1;
+
+    cout << "\nEnter month: ";
+    cin >> month3;
+    cout << "Enter rainfall for " << month3 << ": ";
+    cin >> rainfall1;
+
+
+}
+
+
+
+
+
