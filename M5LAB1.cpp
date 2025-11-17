9fcb16d42a419b4bc711aaf8a962a81d3ef63a76
Lloffert <sailorlloffert@gmail.com>
2025-11-17 16:45:24 +0000

    Revised M5LAB1 copied structure mkae smth simpler

--- M5LAB1.cpp
+++ M5LAB1.cpp
@@ -6,167 +6,84 @@ Lydia Loffert
 Uses "procedural programming"
 */
 
+
 #include <iostream>
 using namespace std;
 
-// Function declarations
-int  getPlayerChoice(int maxChoice); // let player choose options
-void showChoices(string choice1, string choice2, string choice3);  // display the player choice menus
-// Story choices
-void game_start();
-const int MAX = 3; // max possible choices per node
-void merry_go_round();
-void house(); // this was called "white_room" in the previous version, if error check if you accidentally called it that again
-void taxi();
-void waitRoom();
-void office();
-void gameOver();
-void endCard();
+// Function Prototypes
+// Any functions you make will need to
+// be listed here, in this format, at the top.
+// This is so the compiler knows to save
+// room for them in the final program.
 
+void main_menu();
+void choice_front_door();
+void choice_back_door();
+void choice_go_home();
+// TODO: add more choices here
 
-// main()
 int main() {
-    /*
-    int choice;
-    int max = 3;
-    showChoices("1","2","3");
-    choice = getPlayerChoice(max);
-    cout << "You chose: " << choice << endl;
-    */
-    // Start the game
-    game_start();
-
-
-    // ending
-    return 0;
+  cout << "M5LAB1 - Choose Your Own Adventure" << endl;
+  // load up the main menu
+  main_menu();
+  // when we return here, we're done
+  cout << "Thanks for playing!" << endl;
+  return 0; // finished with no errors
 }
 
-// Function definitions
-/**
- * Get a valid choice from the player.
- * example: if maxChoice is 3, they can choose 1, 2, or 3.
- */
-int getPlayerChoice(int maxChoice) {
-    int choice;
-    while (true) {
-        cout << "\nYour choice: ";
-        cin >> choice;
-        // You can add extra validation if you want.
+void main_menu() {
+  // Write a simple menu that lets the user choose 1,2, or 3, or 4 to quit.
 
-        // Validate range 
-        if (choice >= 1 && choice <= maxChoice) {
-            return choice; // same number they enter
-        }
+  cout << "Main Menu" << endl;
+  cout << "You're in front of a spooky old house..." << endl;
+  cout << "Do you:" << endl;
+  cout << "1. Try the front door" << endl;
+  cout << "2. Sneak around back" << endl;
+  cout << "3. Forget it, and go home" << endl;
+  cout << "4. [Quit]" << endl;
+  cout << "Choose: ";
 
-        cout << "Please choose between 1 and " << maxChoice << ".\n";
-    }
+  int choice;
+  cin >> choice;
+  if (1 == choice) {
+    choice_front_door();
+  } else if (2 == choice) {
+    // call choice 2 here
+  } else if (3 == choice) {
+    // call choice 3 here
+  } else if (4 == choice) {
+    cout << "Ok, quitting game" << endl;
+    return; // go back to main()
+  } else {
+    cout << "That's not a valid choice, please try again." << endl;
+    cin.ignore(); // clear the user input
+    main_menu();  // try again
+  }
 }
 
-void showChoices(string choice1, string choice2, string choice3) {
-    // A quick menu. If a choice is empty ("") it's not shown
-    // but there must always be at least one choice.
-    // Example: showChoices("run","fight","hide");
-    cout << "---- MAKE YOUR CHOICE ----" << endl;
-    int num = 1;
-    cout << num << ". " << choice1 << endl;
-    num++;
+// FUNCTION DEFINITIONS
+// OK, we have the prototypes at the top, but
+// now we have to actually write the functions.
+// They go here, after main().
 
-    if (choice2 != "") {
-        cout << num << ". " << choice2 << endl;
-        num++;
-    }
-
-    if (choice3 != "") {
-        cout << num << ". " << choice3 << endl;
-        num++;
-    }
-
+void choice_front_door() {
+  cout << "Try the front door." << endl;
+  cout << "It's locked. " << endl;
+  cout << "Do you:" << endl;
+  cout << "1. Check around back" << endl;
+  cout << "2. Give up and go home" << endl;
+  int choice;
+  cout << "Choose: ";
+  cin >> choice;
+  if (1 == choice) {
+    choice_back_door();
+  } else if (2 == choice) {
+    choice_go_home();
+  }
 }
 
-//////////////////////////////////////////////////////////
-// Story-related functions
-// Each story choice leads to a new function
-// with new text, and new choices.
-//////////////////////////////////////////////////////////
+void choice_back_door() { cout << "TODO: Write something here" << endl; }
 
-// Game start
-void game_start() {
-    /* Every node of the story does this:
-        - describe what's going on using the R"(message)" 
-                cout method.
-        - showChoices() with up to 3 strings included
-        - getPlayerChoice() with the max set to up to 3
-        - if statements to branch to other nodes
+void choice_go_home() { cout << "TODO: Write something here" << endl; }
 
-    */
-   int choice;
-   // R for Raw -- print every line as is until it ends
-   cout << R"(You wake up on the side of the road, unsure of how you got there. There's no cars in sight. 
-It's an empty street riddled with pot holes, one of which you are currently standing in. 
-Unsure of how to get home, you look to the horizon and see a lone merry-go-round off in the 
-distance, and far in the other direction is a small cube-like house.
-
-First of all you step out of that pothole, but after that where do you go?
-    
-)";
-    // show choices
-    showChoices("The merry-go-round.",
-                "The house",
-                "PLACEHOLDER");
-    // make choices
-    choice = getPlayerChoice(MAX);
-    // branch from here
-    // in your program, call another story function!
-    if (choice == 1) {
-        cout << "You make your way north towards the merry-go-round.\n" << endl;
-        merry_go_round(); //GOTO merry_go_round()
-    }
-    if (choice == 2) {
-        cout << "You head towards the house.\n" << endl;
-        house(); // GOTO house()
-    }
-    if (choice == 3) {
-        cout << "PLACEHOLDER\n" << endl;
-    }
-}
-
-void merry_go_round() {
-    cout << R"(As you approach it you see it's a bit run down, and seems to be the only attraction here 
-for miles. The only other person here is the attendant skulking by the ride, having a smoke. 
-He regards you briefly but doesn't speak.
-
-"Hello", You say, "I'm lost, could you tell me where I am?"
-
-"Don't ask me, did you want to ride the merry-go-round?" He clearly doesn't seem to care 
-much about your predicament. "It's only a quarter," he adds. "And it's not very fun." he 
-adds again. As you're about to decline he cuts in a third time "-Also it's broken right now 
-you can't ride it". He lets out a puff of smoke politely away from your face. "You can spin 
-around in a circle here if you want though, and you can do it for free".
-
-Well, wnat to humor him? 
-
-)";
-
-    showChoices("Spin around in a circle.",
-                "PLACEHOLDER",
-                "This guy's weird let's go back...");
-
-    choice = getPlayerChoice(MAX);
-
-    if (choice == 1) {
-        cout << "You make your way north towards the merry-go-round.\n" << endl;
-        merry_go_round(); //GOTO merry_go_round()
-    }
-    if (choice == 2) {
-        cout << "You head towards the house.\n" << endl;
-        house(); // GOTO house()
-    }
-    if (choice == 3) {
-        cout << "PLACEHOLDER\n" << endl;
-    }
-
-}
-
-void house() {
-    cout << R"()";
-}
+// any new choices go here
