6af61f969ec188b97ca94171c40ed8c234b67be5
Lloffert <sailorlloffert@gmail.com>
2025-11-19 16:49:22 +0000

    Question3 done Bronze version completed

--- M5HW1.cpp
+++ M5HW1.cpp
@@ -15,7 +15,7 @@ void Question3();
 
 int main() {
 
-    // Question1();
+    Question1();
     Question2();
     Question3();
 
@@ -110,7 +110,57 @@ Input Validation: no side can be zero or less.
 }
 
 void Question3() {
-    cout << "PLACEHOLDER";
+    /*
+    Write a program that asks the user to enter a number within the range of 1 through 10. Use either 
+    if statements, or a switch statement, to display the Roman numeral version of that number.
+    Input Validation: Do not accept a number less than 1 or greater than 10. 
+
+    Sample Run
+    Enter a number (1 - 10): 7↵
+    The Roman numeral version of 7 us VII.↵
+    */
+
+    int userNum;
+
+    cout << "Enter a number (1 - 10): ";
+    cin >> userNum;
+
+    // Switch to print corresponding roman numeral
+
+    switch(userNum) {
+        case 1:
+            cout << "The Roman numeral version of " << userNum << " is I.\n\n";
+            break;
+        case 2:
+            cout << "The Roman numeral version of " << userNum << " is II.\n\n";
+            break;
+        case 3:
+            cout << "The Roman numeral version of " << userNum << " is III.\n\n";
+            break;
+        case 4:
+            cout << "The Roman numeral version of " << userNum << " is IV.\n\n";
+            break;
+        case 5:
+            cout << "The Roman numeral version of " << userNum << " is V.\n\n";
+            break;
+        case 6:
+            cout << "The Roman numeral version of " << userNum << " is VI.\n\n";
+            break;
+        case 7:
+            cout << "The Roman numeral version of " << userNum << " is VII.\n\n";
+            break;
+        case 8:
+            cout << "The Roman numeral version of " << userNum << " is VIII.\n\n";
+            break;
+        case 9:
+            cout << "The Roman numeral version of " << userNum << " is IX.\n\n";
+            break;   
+        case 10:
+            cout << "The Roman numeral version of " << userNum << " is X.\n\n";
+            break;
+        
+    }
+
 }
 
 
