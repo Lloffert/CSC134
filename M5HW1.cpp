258393c60e64c4dee04a5beb0a5e5d97d0af4425
Lloffert <sailorlloffert@gmail.com>
2025-11-19 16:25:34 +0000

    Q1 and 2 work but no input validation yet

--- M5HW1.cpp
+++ M5HW1.cpp
@@ -9,13 +9,26 @@ Lydia Loffert
 #include <iomanip> // for setprecision
 using namespace std;
 
+void Question1();
+void Question2();
+void Question3();
+
 int main() {
+
+    // Question1();
+    Question2();
+
+
+    return 0;
+}   
+
+void Question1() {
 
     // Question 1: 
     // *Calculate average rainfall for three months,        
     // *asks user for the name of each month                $    
     // *ask amount of rain (in inches) that fell each month $
-    // *display average (rounded to 2 decimal points)       
+    // *display average (rounded to 2 decimal points)    
 
     cout << "QUESTION 1\n";
     cout << "----------\n\n";
@@ -51,6 +64,40 @@ int main() {
     cout << "\nThe average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << setprecision(3) << rainAvg << " inches.\n\n";
 
 }
+
+void Question2() {
+/*
+Write a program that calculates the volume of a three dimensional hypperrectangle.  
+Ask the user to enter width, length, and height for the block. These values should be of type 
+double. Calculate the volume of the block. 
+Input Validation: no side can be zero or less. 
+*/ 
+    cout << "QUESTION 2\n";
+    cout << "----------\n\n";
+
+    // declare variables
+    double width;
+    double length;
+    double height;
+    double volume;
+
+    // Get dimensions from user
+    cout << "Enter rectangle width: ";
+    cin >> width;
+
+    cout << "Enter rectangle length: ";
+    cin >> length;
+
+    cout << "Enter rectangle height: ";
+    cin >> height;
+
+    // calculate volume
+    volume = length * width * height;
+    cout << "The volume of the rectangle is: " << volume << " inches\n\n";
+
+}
+
+
 
 
 
