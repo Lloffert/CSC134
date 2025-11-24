5eca5a2e066405276ed3b8c74976f1f9992eccc7
Lloffert <sailorlloffert@gmail.com>
2025-11-24 16:51:12 +0000

    Got kinda bad skeleton

--- M6HW1.cpp
+++ M6HW1.cpp
@@ -1,32 +1,37 @@
 /*
 CSC 134
-M6HW1 - Project Prototype
+M6HW1 - Silver
 Lydia Loffert
 11/24/2025
 */
 
-/*
-DIRECTIONS:
----------- 
-
-*Bronze:*
-Game should have at least three different rooms, which have exits between them. 
-(Or, if you don't use rooms, at least three different "decision points" or actions.)
-
-*Silver:*
-Bronze, plus some dialog (either signs or NPCs) explaining what the 'point' of the 
-game is, and some interaction (such as picking up items)
-
-*Gold:*
-Silver, plus the following:
-Some kind of ending
-At least one lock and key situation
-For example, this means that a game where you have to find a key on one screen to 
-open the door on a different screen to escape would count as Gold for full credit.
-
-You could do this as finding a note one place that gives you the combination for a 
-safe, or many other methods. Whatever fits with your game and your "foo"!
-*/
-
 #include <iostream>
 using namespace std;
+
+/*
+NEEDS:
+-----
+
+* at least 3 rooms
+* some dialogue + NPCs explaining point of game & how to play
+* lock & key situation
+* ending
+
+*/
+
+void room1();
+void room2();
+void room3();
+
+int main() {
+    cout << "PLACEHOLDER MAIN";
+    room1();
+}
+
+void room1() {
+    cout << "PLACEHOLDER THIS IS ROOM 1";
+    cout << "CHOOSE WHERE TO GO: ";
+    
+
+}
+
